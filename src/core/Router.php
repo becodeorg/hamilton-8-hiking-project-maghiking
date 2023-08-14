4e73d637e3e94c8aca2b91c2968e026ec844aa6d
Robin <robin.t@live.be>
2023-08-14 10:12:45 +0200

    project setup

---
+++ src/core/Router.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace core;
+
+class Router
+{
+    public function route(string $uri_path): void
+    {
+        switch ($uri_path) {
+            case "/":
+            case "/index":
+            case "/home":
+                echo "It works!";
+                break;
+        }
+    }
