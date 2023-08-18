2a84d269b15e702851679380b2b1b34ad50f5ed5
Lucie Monteiro <lucie.mtro@gmail.com>
2023-08-18 12:50:52 +0200

    ADD style

--- src/views/index.view.php
+++ src/views/index.view.php
@@ -4,11 +4,13 @@
         foreach ($hikes as $hike):
             extract($hike); ?>
             <div class="card">
-                <img width="300px" src="<?= $image_url ?>" alt="photo-rando">
-                <h4><a href="/hike?hid=<?= $hid ?>"><?= $name ?></a></h4>
-                <p><?= $distance ?> km</p>
-                <p><?= $duration ?> <i class="fa-solid fa-clock"></i></p>
-                <a href="/profile?uid=<?= $uid ?>"><i class="fa-solid fa-user"></i> <?= $nickname ?></a>
+                <img src="<?= $image_url ?>" alt="photo-rando">
+                <div>
+                    <h4><a href="/hike?hid=<?= $hid ?>"><?= $name ?></a></h4>
+                    <p><?= $distance ?> km</p>
+                    <p><?= $duration ?> <i class="fa-solid fa-clock"></i></p>
+                    <a href="/profile?uid=<?= $uid ?>"><i class="fa-solid fa-user"></i> <?= $nickname ?></a>
+                </div>
             </div>
         <?php endforeach;
     else: ?>
